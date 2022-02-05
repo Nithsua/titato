c582d76098d6025fbbe086468647ead5a08cefac
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-02-06 01:21:34 +0530

    test for game play

--- tests/titato.ts
+++ tests/titato.ts
@@ -1,7 +1,16 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { Address, Program } from '@project-serum/anchor';
 import { Titato } from '../target/types/titato';
 import { expect } from 'chai';
+
+const play = async (program: Program<Titato>, game: Address, player, tile, expectedTurn, expectedGameState, expeectedBoard) => {
+  await program.rpc.play(tile, { accounts: { player: player.publicKey, game }, signers: player instanceof (anchor.Wallet as any) ? [] : [player] });
+
+  const gameState = await program.account.game.fetch(game);
+  expect(gameState.turn).to.equal(expectedTurn);
+  expect(gameState.state).to.eql(expectedGameState);
+  expect(gameState.board).to.eql(expeectedBoard);
+}
 
 describe('titato', () => {
 
@@ -15,11 +24,27 @@ describe('titato', () => {
     const playerOne = program.provider.wallet;
     const playerTwo = anchor.web3.Keypair.generate();
     await program.rpc.setupGame(playerTwo.publicKey, { accounts: { game: gameKeypair.publicKey, playerOne: playerOne.publicKey, systemProgram: anchor.web3.SystemProgram.programId }, signers: [gameKeypair] })
-    
+
     let gameState = await program.account.game.fetch(gameKeypair.publicKey);
     expect(gameState.turn).to.equal(1);
     expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey]);
     expect(gameState.state).to.eql({ active: {} });
     expect(gameState.board).to.eql([[null, null, null,], [null, null, null,], [null, null, null,]]);
+  });
+
+  it('player one wins', async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = program.provider.wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+
+    await program.rpc.setupGame(playerTwo.publicKey, { accounts: { game: gameKeypair.publicKey, playerOne: playerOne.publicKey, systemProgram: anchor.web3.SystemProgram.programId }, signers: [gameKeypair] });
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players).to.eql([playerOne.publicKey, playerTwo.publicKey]);
+    expect(gameState.state).to.eql({ active: {}, });
+    expect(gameState.board).to.eql([[null, null, null], [null, null, null], [null, null, null]]);
+
+    await play(program, gameKeypair.publicKey, playerOne, { row: 0, column: 0 }, 2, { active: {}, }, [[{ x: {}}, null, null],[null, null, null],[null, null, null]]);
   });
 });
